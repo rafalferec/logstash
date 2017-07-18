436c05f6fe90e7cd75677989be5ae171da78f693
rafalferec <rafal.ferec@gmail.com>
2017-07-18 11:24:57 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,13 +3,13 @@ set -e
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
-	set -- logstash -f /usr/logstash-settings/logstash.conf "$@"
+	set -- logstash"$@"
 fi
 
 # Run as user "logstash" if the command is "logstash"
 # allow the container to be started with `--user`
 if [ "$1" = 'logstash' -a "$(id -u)" = '0' ]; then
-	set -- gosu logstash "$@"
+	set -- gosu logstash -f /usr/logstash-settings/logstash.conf "$@"
 fi
 
 exec "$@"
