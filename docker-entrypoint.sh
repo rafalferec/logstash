f7f22d0d8dfb0824ce33c09b713213644774999f
rafalferec <rafal.ferec@gmail.com>
2017-07-18 11:00:42 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
-	set -- logstash -f /etc/logstash/conf.d/logstash.conf "$@"
+	set -- logstash -f /usr/logstash-settings/logstash.conf "$@"
 fi
 
 # Run as user "logstash" if the command is "logstash"
